ac5f6aad50821ffcadc4cdba1fc5d95e62d5e508
Saurabh <saurab@techbrise.com>
2018-11-27 16:30:38 +0530

    first commit

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wpmu');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'techbrise');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
